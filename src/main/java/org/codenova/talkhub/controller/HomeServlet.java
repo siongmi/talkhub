272d4a78d6ca13e92f56581be322de6ae5a4a855
siongmi <tldmseeo@naver.com>
2025-03-05 11:20:13 +0900

    feat: 인증여부에 따른 view 변경

--- src/main/java/org/codenova/talkhub/controller/HomeServlet.java
+++ src/main/java/org/codenova/talkhub/controller/HomeServlet.java
@@ -5,6 +5,7 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
 
@@ -14,14 +15,21 @@ public class  HomeServlet extends HttpServlet {
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+        HttpSession session = req.getSession();
+        String sid = session.getId();
+        boolean f = session.isNew();
+        long ct = session.getCreationTime();
+
+        System.out.println("session info ==============");
+        System.out.println("sessionId" + sid);
+        System.out.println("isNew" + f);
+        System.out.println("createTime" + ct);
+
         if(Math.random()>0.5){
             req.setAttribute("authentication", true);
-
         }else{
             req.setAttribute("authentication", false);
-
         }
-
         req.getRequestDispatcher("/WEB-INF/views/index.jsp").forward(req, resp);
     }
 }
